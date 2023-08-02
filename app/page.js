0f6113f0ac8bd1da86b5d280643db4651f4e8381
Qagura <quddusnuriman@gmail.com>
2023-08-03 01:56:08 +0800

    api key testing

--- app/page.js
+++ app/page.js
@@ -3,8 +3,11 @@
 import localFont from "next/font/local";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import "dotenv/config";
 
 const remBold = localFont({ src: "../public/fonts/REM-Bold.ttf" });
+
+// console.log(process.env.API_KEY);
 
 export default function Home() {
   const [placeholder, setPlaceholder] = useState("eggs, banana, chicken...");
@@ -22,7 +25,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center p-24 bg-white dark:bg-primary text-secondary dark:text-slate-200">
       <h1 className={` ${remBold.className} text-4xl mb-10`}>
-        What's in your fridge today?
+        What&apos;s in your fridge today?
       </h1>
       <input
         title="test"
